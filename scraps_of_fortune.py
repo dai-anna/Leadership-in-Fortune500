bfc6906a13ce4b8055c151f595452120e0ae28e3
ad522 <ad522@duke.edu>
2021-10-16 01:20:44 -0400

    upload backups

--- scraps_of_fortune.py
+++ scraps_of_fortune.py
@@ -9,34 +9,73 @@ import json
 # To scrape Fortune 500 for full list of companies
 MAINSCRAPER = False
 if MAINSCRAPER:
+    cookies = {
+        '_ga': 'GA1.2.488745781.1634319237',
+        '_gid': 'GA1.2.1919468814.1634319237',
+        '_gcl_au': '1.1.554956561.1634319240',
+        'usprivacy': '1---',
+        'notice_behavior': 'none',
+        'pv': '21',
+        '__tbc': '%7Bjzx%7DMKUmQj7fgSDqlMRYpGnQ-CDyTKMMMdOBsCmmy1UTGYZGgHgIsO6eBCQt3VdMOy9xn8PsXyAYn9hj4_1gchHLOSydw0CYcpUIempCW6XQ4_8',
+        '__pat': '-18000000',
+        '__pvi': '%7B%22id%22%3A%22v-kuta1x5rr4opy8bo%22%2C%22domain%22%3A%22.fortune.com%22%2C%22time%22%3A1634357188987%7D',
+        'xbc': '%7Bjzx%7DkxtyMu2hraH3zN_sGn4xBnIlSvzL-8tvbnJ71aU0Fdg_UPiDMXzNBnR0lXdd3vnnbyaw_38nEDvs1eziJy0Wg4Mv0Oj4dXATZVLcSxFGM-xK__SOhJ7CEdO0B9nmSVT5YQCSAhqIy9axi6ZfsIkHNr0w6g__cbm5uC2SGEzqkc8GkX_s3VjCcGzTRH4d-1i28Uo1sJvaxk15ckPldU3aLqWKVRkovIdtszxYIr1WbF5lnV_1jgOJrqPH_1x44XzLAIB1yck0AbC1b6e4mrfVde-a9cqsKBPcmUqsCW5qy-gYpRp4RAUN35D7DfBfNs4Cj-jpgecoNBuyhK2PDrMZUgmts_U4JxLxtuv7_ftwC9_y_YKuBmgZnOff4-n5BKYW9wvS8kN4BdWR1aJiG0dfMbGpQP8f_3SfMG0zWTKmSYhZahUpFVCKftqoswHcQcKalmQMjerZRq1qDSOm5qUNbwmxaPbJGmuqFuuOUzwxOeDcQY8fO6ZvQvmNM3BNe9FQgi6IfWkGUnRy-zO87xwIvQjnnd8JjfV29LTv-Fj0AJIhfuvn1xOfz4mYevCsY-56QlV2gLLpWOw2EQrW2xRwAk36THnS_qLSz80F68-X6R4-ZJrJT6Ocb61_8MTq_NrXdWaKZ-2mrOIpYcsXzEJn_RROhvskKVPIt98muoD2r16WbxDLuYNEHDF5EqN5eyRcCm8ctNfMunLtVwt7CAchYZW5uOxpiwKUGatUwIQs-NDthgD57T6T3aRNINIfhYUMgq_UvjfsuwE4Tom3Tyyeg4FJwIrVcNvofIe7Y8IpX9HiANx1zo5L-9eoAar0IxwO',
+        '_pc_accessLevel': 'none',
+        '_pc_segment': 'fcasual',
+        '_parsely_visitor': '{%22id%22:%22pid=203f7a32f0b69cd6515734152c800bea%22%2C%22session_count%22:5%2C%22last_session_ts%22:1634357156615}',
+        'ntv_as_us_privacy': '1---',
+        '_ntv_uid': '95d87721-031a-44ff-b12c-2346c23c7606',
+        'cX_S': 'kusnhawqd93oiggu',
+        '_fbp': 'fb.1.1634319245040.2018298079',
+        '__gads': 'ID=432ccdb44bf14037-223cef61a4cc0058:T=1634319244:S=ALNI_Mb0OrbzaCfL1qVUL4hMaEMxiuDqpQ',
+        'cX_G': 'cx%3A3odt62vp7ehi23v6vnwf6m2xbl%3A60h1hrp4cj2v',
+        '__pil': 'en_US',
+        '_gac_UA-142764551-3': '1.1634357151.EAIaIQobChMI2PHN6IbO8wIV0jArCh2v7QC9EAAYASAAEgIn6_D_BwE',
+        '_gcl_aw': 'GCL.1634357152.EAIaIQobChMI2PHN6IbO8wIV0jArCh2v7QC9EAAYASAAEgIn6_D_BwE',
+        '_gac_UA-97981691-5': '1.1634330385.EAIaIQobChMIqK35iaPN8wIVjZZLBR0nUgoaEAAYASAAEgLTefD_BwE',
+        'tpcc_gfortune500': '%7B%22date%22%3A1634330380497%7D',
+        'ntvSession': '{}',
+        '_parsely_session': '{%22sid%22:5%2C%22surl%22:%22https://fortune.com/fortune500/%22%2C%22sref%22:%22https://www.google.com/%22%2C%22sts%22:1634357156615%2C%22slts%22:1634335200834}',
+        '_uetsid': '150840902dde11ec9a643b0826a18087',
+        '_uetvid': '15086aa02dde11ec82e4739b2f623639',
+        'iris_user_id': 'UP-uOJaalANkmTGILy',
+        '_gat_gaTracker': '1',
+        '_gat_pianoTracker': '1',
+        '_gat_fortuneAnonymized': '1',
+    }
+
     headers = {
-        "authority": "fortune.com",
-        "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        "accept": "application/json",
-        "authorization": "Basic Zm9ydHVuZTpCcHNyZmtNZCN5SndjWkkhNHFqMndEOTM=",
-        "sec-ch-ua-mobile": "?0",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-dest": "empty",
-        "referer": "https://fortune.com/fortune500/2021/search/",
-        "accept-language": "en-US,en;q=0.9,fr;q=0.8,zh-CN;q=0.7,zh;q=0.6",
-        "cookie": "_ga=GA1.2.595368620.1634259110; _gid=GA1.2.567901930.1634259110; _gcl_au=1.1.436531842.1634259111; usprivacy=1---; notice_behavior=none; __pat=-18000000; _pc_accessLevel=none; _pc_segment=fcasual; cX_S=kurnol12efc8x9yn; cX_G=cx%3A32n7ms9w3h3j81j8z8hnwc16ys%3Agoccpo0vgdgu; __tbc=%7Bjzx%7DMKUmQj7fgSDqlMRYpGnQ-FEqDAJmXSDJHMaGahx7RgZZmdh3A9Rk_RvIWSDaqnrjvrdJCt3ncmOBiNngoG7Glyydw0CYcpUIempCW6XQ4_8; __pil=en_US; _parsely_session={%22sid%22:2%2C%22surl%22:%22https://fortune.com/fortune500/?utm_content=invest&tpcc=gfortune500&gclid=CjwKCAjwzaSLBhBJEiwAJSRoklu05EaclOXmG30J7MqkduZOuoRYaMDckHlKzx9G95Q-gspElM2XexoCHDwQAvD_BwE%22%2C%22sref%22:%22https://www.google.com/%22%2C%22sts%22:1634314915319%2C%22slts%22:1634259116427}; _parsely_visitor={%22id%22:%22pid=59d2531d52aa18af2680cc2beb01b468%22%2C%22session_count%22:2%2C%22last_session_ts%22:1634314915319}; _gac_UA-142764551-3=1.1634314968.CjwKCAjwzaSLBhBJEiwAJSRoklu05EaclOXmG30J7MqkduZOuoRYaMDckHlKzx9G95Q-gspElM2XexoCHDwQAvD_BwE; _gcl_aw=GCL.1634314969.CjwKCAjwzaSLBhBJEiwAJSRoklu05EaclOXmG30J7MqkduZOuoRYaMDckHlKzx9G95Q-gspElM2XexoCHDwQAvD_BwE; tpcc_gfortune500=%7B%22date%22%3A1634314968965%7D; _gac_UA-97981691-5=1.1634314972.CjwKCAjwzaSLBhBJEiwAJSRoklu05EaclOXmG30J7MqkduZOuoRYaMDckHlKzx9G95Q-gspElM2XexoCHDwQAvD_BwE; pv=5; _uetsid=175eef002d5211ec9dc383d1759b5977; _uetvid=175f30002d5211ec8db3a519779f4c20; __pvi=%7B%22id%22%3A%22v-kusky6xof1bnnsar%22%2C%22domain%22%3A%22.fortune.com%22%2C%22time%22%3A1634314992540%7D; xbc=%7Bjzx%7DNuaZ6oxUgMrlbNDVwtnF6-HafIkspF_VCEI1CdL1KYJ8OEySB1r_cmgRDfxsqxqJhJAmxc_0AysWdOvV6iNVZMqFZb6MhoB7N9lBK7S0jJcsDsNThoP3k1cCJv79rZHetI2OWpegN5-YPRW8PXFUBG0gpshCxSvN_mZI69XubOShtAZehGZWJ0ckOPBFaaaKWsKbEmUFwZGmfU9xQ8fS-vGpUdqbRzOYPnYW8m8h9OudHaocYtB6DF6jFJoEKKYg1mpk18_msbpr7gQtnayGv6iR_Tyew0zv4rsuAak_ZILk7KNEZTmXqnYTOG4ntosxX4X0NtzejYu77kUkEVs5dEi_97uijpVBu_1bC7aKfqYN8vbA86FhRhRljbHOGJbvskXlWGW0DRgftEPgaB_oKaQasw87oOtCNoeu7COrDENxc2i4BeuTJBMNzDzaSnv610yRi_q4hqJSDKSHUYThFP0OClHLQ86-aiGAdYOcgCjWRwPxJB6dx8tpofRRfz-N-6GzYu6Wh9TrpM54CrtH9dNPBI1PevjsTS42ZAJXPxlUz2vplZYHonrzf7oRUhQkOo9N5hBfKYJA-sOA3Oz_P02GkC_VAnSIyUKE6L5qLB0wS9n2OqcU2yKwH2yZNqRYbAofjxsvF0Y47FK4_yuEwRk8lO6s2nOMkCAO9kNgmqWwVqoHf82UvBSehNdUkBOi26kTjPKhsjozRtnwp0MaBiZqmHdILmNqD8YZaNqgHGc0TO37-N2jeOX1N_Ir6pHebkqAHDGR7_YqgS4BuAJikK-Fe44vcq3c7B2DxdQhjMXwr1KKddBxsmzljzIxD8Ny; _gat_pianoTracker=1; _gat_fortuneAnonymized=1",
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://fortune.com/fortune500/2021/search/',
+        'Authorization': 'Basic Zm9ydHVuZTpCcHNyZmtNZCN5SndjWkkhNHFqMndEOTM=',
+        'Connection': 'keep-alive',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
     }
 
     params = (
-        ("list_id", "3040727"),
-        ("token", "Zm9ydHVuZTpCcHNyZmtNZCN5SndjWkkhNHFqMndEOTM="),
+        ('list_id', '3040727'),
+        ('token', 'Zm9ydHVuZTpCcHNyZmtNZCN5SndjWkkhNHFqMndEOTM='),
     )
 
-    response = requests.get(
-        "https://fortune.com/wp-json/irving/v1/data/franchise-search-results",
-        headers=headers,
-        params=params,
-    )
+    response = requests.get('https://fortune.com/wp-json/irving/v1/data/franchise-search-results', headers=headers, params=params, cookies=cookies)
 
-    main_site = response.json()  # main_site is a list of two dictionaries
+
+    main_site = response.json()
+
+#%%
+# To save to disk
+if MAINSCRAPER:
+    with open("initial_scrape.json", "w") as f:
+        json.dump(main_site, f)
+
+#%%
+# To load from disk
+with open("initial_scrape.json", "r") as f:
+        main_site = json.load(f)
 
 #%%
 headers_dict = main_site[0]
@@ -134,10 +173,14 @@ result_df = pd.read_csv("results_tickers.csv")
 
 #%%
 # To view the remaining nested data in a better format
-VIEWINPRETTYJSON = False
-if VIEWINPRETTYJSON:
+VIEWINJSON = False
+if VIEWINJSON:
     with open("dump.json", "w") as f:
         json.dump(data_list, f)
+
+
+with open("dump.json", "r") as f:
+        hihi = json.load(f)
 
 #%%
 # To unnest my data and build a dictionary for each company
@@ -166,18 +209,32 @@ company_df = pd.merge(
     right_on="url",
     validate="1:1",
     indicator=True,
-).drop(["permalink","Unnamed: 0"], axis=1)
-company_df
+).drop(["permalink", "Unnamed: 0"], axis=1)
 
 #%%
 # To clean up company_df column variable type
 
-convert_to_int = ["ranking", "rankchange1000", "f500_employees", "rankchange", "measure-up-rank"]
-convert_to_float = ["f500_revenues","revchange", "f500_profits", "prftchange", "assets", "f500_mktval"]
+convert_to_int = [
+    "ranking",
+    "rankchange1000",
+    "f500_employees",
+    "rankchange",
+    "measure-up-rank",
+]
+convert_to_float = [
+    "f500_revenues",
+    "revchange",
+    "f500_profits",
+    "prftchange",
+    "assets",
+    "f500_mktval",
+]
 company_df[convert_to_int] = company_df[convert_to_int].replace("", str(0))
 company_df[convert_to_float] = company_df[convert_to_float].replace("", str(0))
 company_df[convert_to_int] = company_df[convert_to_int].astype("int")
 company_df[convert_to_float] = company_df[convert_to_float].astype("float")
+# company_df.loc["revchange", "prftchange"] = company_df.loc["revchange", "prftchange"]/100
+company_df.sample(10)
 
 #%%
 # Creating a database in sqlite
